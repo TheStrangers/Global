f5e826d65d8d51bbc2006ea574c6c229de6ce484
Rolliks <Torr-a@mail.ru>
2015-01-25 21:46:02 +0400

    Schetchic1

---
+++ Study/Study/Schetchic.h
@@ -0,0 +1,22 @@
+
+
+using namespace std;
+
+class CSchetchic
+{
+private:
+	unsigned int sch;
+public:
+	CSchetchic() :sch(0)
+	{}
+	void GetVar()
+	{
+		return sch;
+	}
+	void PlusVar()
+	{
+		sch++;
+	}
+
+
+};
--- Study/Study/main.cpp
+++ Study/Study/main.cpp
@@ -1,35 +1,18 @@
 ﻿#include "Headers.h"
 #include "Semaphore.h"
 #include "Test.h"
+#include "Schetchic.h"
 
 int main()
 {
-	int col_in[3];
-	CSemaphore *Signal1 = new CSemaphore();
-	CYashic *Test = new CYashic;
+    CSchetchic s1, s2;
+	s1.GetVar();
+	s2.GetVar();
 
-	printf("Start working.\n");
-	Signal1->Output();
-
-	printf("Set red light. Checking...\n");
-	Signal1->SetR();
-	Signal1->Output();
-
-	printf("Set yellow light. Checking...\n");
-	Signal1->SetY();
-	Signal1->Output();
-
-	printf("Set green light. Checking...\n");
-	Signal1->SetG();
-	Signal1->Output();
-
-	printf("Overloading semaphore. Checking...\n");
-	col_in[0] = 1;
-	col_in[1] = 1;
-	col_in[2] = 1;
-	Signal1->SetStatus(*col_in);
-	Signal1->Output();
-
-	printf("Checking done.\n");
+	s1.PlusVar();
+	s2.PlusVar();
+	s2.PlusVar();
 	return 0;
+
+
 }
