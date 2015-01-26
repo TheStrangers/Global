1718416277f16795b3784b678e004322ee65e77c
Rolliks <Torr-a@mail.ru>
2015-01-26 21:50:25 +0300

    Map2

--- Study/Study/Fmanager.cpp
+++ Study/Study/Fmanager.cpp
@@ -1,28 +1,14 @@
+#include "Headers.h"
+
 void GetMap()
 {
-	File *f;
-	f = fopen(new char[10],br);
+	FILE *f;
+	f = fopen(*char,"br");
 	fread(&dat,sizeof(char),1,f);
 	fclose(имя логического файла);
 }
 
 void SetMap()
 {
-	for (i = 1; i < n; i++){
-		for (j = 1; j < m; j++) {
 
-			if (char map[i,j] = ".")
-			{
-				int map1[i, j] = 0;
-			}
-			else if (char map[i, j] = "0")
-			{
-				int map[i, j] = 1;
-			}
-			else if (char map[i, j] = "X")
-			{
-				int map[i, j] = 2;
-			}
-		}
-	}
 }
